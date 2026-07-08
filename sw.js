40f49c1d708d700ea3e6fa17a60a89674ab73f3c
Khaleed Mahmoud <gplinkrecruitment@Khaleeds-MacBook-Air.local>
2026-07-09 00:30:01 +1000

    Career: probe parent-centre element (find app-shell overlay eating wheel)
    The centre-element probe ran inside the iframe, so it can't see a parent
    element covering the iframe. Add a parent elementsFromPoint(centre) probe to
    reveal if something in the app-shell is on top of the active frame and eating
    wheel/pointer events. sw.js VERSION i->j. Temporary.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 (function () {
   "use strict";
 
-  var VERSION = "20260708i";
+  var VERSION = "20260708j";
   var STATIC_CACHE = "gp-link-static-" + VERSION;
   var PAGE_CACHE = "gp-link-pages-" + VERSION;
   var RUNTIME_CACHE = "gp-link-runtime-" + VERSION;
